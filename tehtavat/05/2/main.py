921269fadc739251b7950d33093dd8a2a61fe5c4
Mikko Varis <mikko.varis2@gmail.com>
2024-08-20 23:58:43 +0300

    Cadd changes

--- tehtavat/05/2/main.py
+++ tehtavat/05/2/main.py
@@ -3,6 +3,6 @@ num = input("Anna luku: ")
 while num != "":
     nums.append(int(num))
     num = input("Anna luku: ")
-nums.sort()
-print("Isoin numero:", nums[-1])
-print("Pienin numero:", nums[0])
+nums.sort(reverse=True)
+for i in range(5):
+    print(f"#{i+1}", nums[i])
