9ec8e78e76b8d5f47fdfba1ea8f926b34d1fdf7c
Mikko Varis <mikko.varis2@gmail.com>
2024-08-20 15:21:14 +0300

    complete stuff

---
+++ tehtavat/02/2/main.py
@@ -0,0 +1,5 @@
+import math
+
+sade = int(input("Ympyrän säde: "))
+area = math.pi * (sade ** 2)
+print("Pintaala:", area)
---
+++ tehtavat/02/3/main.py
@@ -0,0 +1,5 @@
+kanta = int(input("Suorakulmion kanta: "))
+korkeus = int(input("Suorakulmion korkeus: "))
+
+print("Suorakulmion piiri:", (kanta * 2) + (korkeus * 2))
+print("Suorakulmion pinta-ala:", kanta * korkeus)
