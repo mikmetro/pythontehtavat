d8884a72bbfe80286613224d53a67bbc669a534a
Mikko Varis <mikko.varis2@gmail.com>
2024-10-23 15:04:54 +0300

    12.2

--- tehtavat/12/2.py
+++ tehtavat/12/2.py
@@ -5,5 +5,6 @@ paikkakunta = input("Anna paikkakunta: ")
 
 api_key = os.environ['WEATHER_API_KEY']
 
-r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&appid={api_key}").json()
-print(r)
+r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={paikkakunta}&units=metric&appid={api_key}").json()
+
+print(f"Lämpötila paikkakunnassa {paikkakunta} on {r["main"]["temp"]} celcius astetta")
